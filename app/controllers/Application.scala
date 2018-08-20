b262e073d2edae00fbf3006b45b49a00d274bbd2
zitt <NKtargeting@yandex.ru>
2018-08-20 21:41:19 +0500

    Add attributes for index template and write unfinished form mapping

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,26 @@
 package controllers
 
 import javax.inject._
+import models.Task
 import play.api._
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+
+import models.TaskState.TaskState
 
 @Singleton
-class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class Application @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+
+  val taskForm: Form[TaskForm] = Form {
+    mapping(
+      "title" -> nonEmptyText,
+      "description" -> text
+    )(TaskForm.apply)(TaskForm.unapply)
+  }
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index(Task.getTasks, taskForm))
   }
 
   def newTask = TODO
@@ -17,3 +29,6 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
 
   def deleteTask(id: Int) = TODO
 }
+
+// TODO: добавить в атрибуты состояние и разобраться, как его обрабатывать
+case class TaskForm(title: String, description: String/*, state: TaskState*/)
