e91b311d857e1accbdeeb6a8aa79c4b7fabf05b5
zitt <NKtargeting@yandex.ru>
2018-08-20 22:03:19 +0500

    Add tasks list handling in a template and a simple manually test

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -10,7 +10,7 @@ import models.TaskState._
 case class Task(id: Int, title: String, description: String, state: TaskState)
 
 object Task {
-  def getTasks: List[Task] = Nil // TODO: временное решение до реализации доступа к БД
+  def getTasks: List[Task] = Task(1, "title", "desc", TODO) :: Nil // TODO: временное решение до реализации доступа к БД
   def create(title: String, description: String, state: TaskState) {}
   def delete(id: Int) {}
   def update(id: Int) {}
