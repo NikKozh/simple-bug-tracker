6fced6ee7a0b8f95d50d654e2fb02651cb76e2af
zitt <NKtargeting@yandex.ru>
2018-08-21 20:24:35 +0500

    Add form binding to my custom TaskState enum

--- app/controllers/TaskForm.scala
+++ app/controllers/TaskForm.scala
@@ -1,16 +1,34 @@
 package controllers
 
+import models.TaskState
+import models.TaskState.TaskState
+import play.api.data.{FormError, Forms}
+import play.api.data.format.Formatter
+
 object TaskForm {
   import play.api.data.Forms._
   import play.api.data.Form
 
-  // TODO: добавить в атрибуты состояние и разобраться, как его обрабатывать
-  case class Data(title: String, description: String/*, state: TaskState*/)
+  // TODO: не забыть выяснить, как обрабатывать потом state при загрузке из БД
+  case class Data(title: String, description: String, state: TaskState)
+
+  // TODO: если останется время - досконально разобрать
+  // Адаптированный метод со StackOverflow:
+  implicit def taskStateFormat: Formatter[TaskState] = new Formatter[TaskState] {
+    override def bind(key: String, data: Map[String, String]) =
+      data.get(key)
+        .map(TaskState.withName(_))
+        .toRight(Seq(FormError(key, "error.required", Nil)))
+
+    override def unbind(key: String, value: TaskState) =
+      Map(key -> value.toString)
+  }
 
   val taskForm: Form[Data] = Form {
     mapping(
       "title" -> nonEmptyText,
-      "description" -> text
+      "description" -> text,
+      "state" -> Forms.of[TaskState]
     )(Data.apply)(Data.unapply)
   }
 }
---
+++ app/controllers/TaskStateFormatter.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import models.TaskState
+import models.TaskState._
+import play.api.data.FormError
+import play.api.data.format.Formatter
+
+class TaskStateFormatter {
+  implicit def taskStateFormat: Formatter[TaskState] = new Formatter[TaskState] {
+    override def bind(key: String, data: Map[String, String]) =
+      data.get(key)
+        .map(TaskState.withName(_))
+        .toRight(Seq(FormError(key, "error.required", Nil)))
+
+    override def unbind(key: String, value: TaskState) =
+      Map(key -> value.toString)
+  }
+}
